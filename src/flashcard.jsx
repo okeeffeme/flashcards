83ad572b1869aa0d9456ca309fea2045085aa4bf
okeeffeme <okeeffeme@gmail.com>
2018-07-18 13:46:21 +0200

    Rough work on Flashcard class

---
+++ src/flashcard.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Flashcard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.createAnswer = this.createAnswer.bind(this);
+    this.createAnswers = this.createAnswers.bind(this);
+  }
+
+  createAnswer = answer => (
+    <div>
+      <p>
+        { answer }
+      </p>
+    </div>
+  )
+
+  createAnswers = answers => (
+    answers.map(this.createAnswer)
+  )
+
+  render() {
+    const { props } = this;
+    const { question, answers } = props;
+    return (
+      <div>
+        <h2>{ question }</h2>
+        {this.createAnswers(answers)}
+      </div>
+    );
+  }
+}
+
+Flashcard.propTypes = {
+  question: PropTypes.string,
+  answers: PropTypes.array,
+};
+
+Flashcard.defaultProps = {
+  question: 'Question Default',
+  answers: [
+    'Answer A Default',
+    'Answer B Default',
+  ],
+};
+
+export default Flashcard;
