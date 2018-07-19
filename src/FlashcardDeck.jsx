9a88f0dfffb49dca0a5cd2c8f0fd72670d9e6d7f
okeeffeme <okeeffeme@gmail.com>
2018-07-19 18:46:16 +0200

    Flashcard Deck working

--- src/FlashcardDeck.jsx
+++ src/FlashcardDeck.jsx
@@ -5,14 +5,47 @@ import Flashcard from './Flashcard';
 
 /* eslint-disable-next-line react/prefer-stateless-function */
 class FlashcardDeck extends React.Component {
+  createCard = input => (
+    <Flashcard input={input} />
+  )
+
+  createCards = deck => (
+    deck.map(this.createCard)
+  )
 
   render() {
+    const { deck } = this.props;
     return (
       <div>
-        <h2>TEST</h2>
+        {this.createCards(deck)}
       </div>
     );
   }
 }
+
+FlashcardDeck.propTypes = {
+  deck: PropTypes.arrayOf(Flashcard.propTypes.card),
+};
+
+FlashcardDeck.defaultProps = {
+  deck: [
+    {
+      question: 'Question 1 Default Deck',
+      options: [
+        'Answer A Default Deck',
+        'Answer B Default Deck',
+      ],
+      answer: 'Answer A Default Deck',
+    },
+    {
+      question: 'Question 2 Default Deck',
+      options: [
+        'Answer C Default Deck',
+        'Answer D Default Deck',
+      ],
+      answer: 'Answer C Default Deck',
+    },
+  ],
+};
 
 export default FlashcardDeck;
